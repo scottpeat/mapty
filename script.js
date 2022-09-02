bed477d80ce2cc2fbbb41d17acf573baf06bba31
Scott Peat <scottpeatweb@gmail.com>
2022-09-02 17:58:17 +0100

    Fixing Location error

--- script.js
+++ script.js
@@ -16,16 +16,18 @@ if (navigator.geolocation)
     function (position) {
       const { latitude } = position.coords;
       const { longitude } = position.coords;
-      console.log('http://www.google.co.uk/maps/@${latitude}, ${longitude}');
+      console.log('https://www.google.co.uk/maps/@${latitude}, ${longitude}');
 
-      const map = L.map('map').setView([51.505, -0.09], 13);
+      const map = L.map('map').setView(coords, 13);
+
+      const coords = [latitude, longitude];
 
       L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution:
           '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
       }).addTo(map);
 
-      L.marker([51.5, -0.09])
+      L.marker(coords)
         .addTo(map)
         .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
         .openPopup();
